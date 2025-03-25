b248b89bb614bf708b91e6340020670e1ce17d89
Darshan <darshanmprakash99@gmail.com>
2025-03-24 21:06:54 -0500

    Switch data storage from CSV to SQLite

--- server/app.py
+++ server/app.py
@@ -1,42 +1,53 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 from scraper import process_job_url
-from storage import update_job_status
-import csv
-import os
+from sql_storage import update_job_status
+import sqlite3
 
 app = Flask(__name__)
 CORS(app)
 
-CSV_FILE = "tracker.csv"
+DB_FILE = "tracker.db"
 
-def read_jobs_from_csv():
-    if not os.path.exists(CSV_FILE):
+def read_jobs_from_db():
+    try: 
+        conn = sqlite3.connect(DB_FILE)
+        conn.row_factory= sqlite3.Row
+        cursor = conn.cursor()
+        cursor.execute("SELECT * FROM jobs")
+        rows = cursor.fetchall()
+
+        jobs = []
+        for row in rows:
+            job = {
+                'id': row['id'],
+                'title': row['job_title'],
+                'company': row['company_name'],
+                'location': row['location'],
+                'link': row['job_link'],
+                'date': row['date_applied'],
+                'status': row['status'],
+                'description': row['job_description']
+            }
+            jobs.append(job)
+        return jobs
+    except sqlite3.Error as e:
+        print(f"Database Error,{e}")
         return []
-    
-    jobs = []
-    with open(CSV_FILE, 'r', encoding='utf-8') as f:
-        reader = csv.DictReader(f)
-        for row in reader:
-            jobs.append({
-                'title': row['Job Title'],
-                'company': row['Company Name'],
-                'location': row['Location'],
-                'link': row['Job Link'],
-                'date': row['Date Applied'],
-                'status': row['Status'],
-                'description': row['Job Description']
-            })
-    return jobs
+    finally:
+        if conn:
+            conn.close()
+
 
 @app.route('/getJobs', methods=['GET'])
 def get_jobs():
     try:
-        jobs = read_jobs_from_csv()
+        jobs = read_jobs_from_db()
         return jsonify(jobs)
     except Exception as e:
         return jsonify({'error': str(e)}), 500
 
+# Send job to scraper
 @app.route('/submitJob', methods=['POST'])
 def submit_job():
     try:
@@ -51,21 +62,19 @@ def submit_job():
         return jsonify(job_data)
     except Exception as e:
         return jsonify({'error': str(e)}), 500
-    
+
+# Update status when changes are made in the frontend
 @app.route('/updateStatus', methods=['POST'])
 def update_status():
     try:
         data = request.json
-
-        title = data.get('title')
-        company = data.get('company')
-        date = data.get('date')
+        job_url = data.get('url')
         new_status = data.get('status')
 
-        if not all([title, company, date, new_status]):
+        if not all([job_url, new_status]):
             return jsonify({'error': 'Missing required fields'}), 400
         
-        success = update_job_status(title, company, date, new_status)
+        success = update_job_status(job_url, new_status)
 
         if not success:
             return jsonify({'error': 'Job not found'}), 404
