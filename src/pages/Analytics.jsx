9c9e2e7f0ca49a880adb11a5b07f498e23f9ba3d
Darshan <darshanmprakash99@gmail.com>
2025-04-27 17:14:31 -0500

    Add summary grid, charts

--- src/pages/Analytics.jsx
+++ src/pages/Analytics.jsx
@@ -1,10 +1,25 @@
 import React from "react";
-
+import AnalyticsSummaryGrid from "../components/AnalyticsSummaryGrid";
+import JobStatusBarChart from "../components/JobStatusBarChart";
+import ApplicationsChart from "../components/ApplicationsByDateLineChart";
 
 export default function Analytics() {
-    return(
-        <div>
-            <h1>Analytics</h1>
+  return (
+    <div style={{ 
+      backgroundColor: "#F7F5F0", 
+      minHeight: "100vh", 
+      padding: "20px" 
+    }}>
+      <AnalyticsSummaryGrid />
+      <div className="flex space-x-8 items-center justify-center">
+        <div className="flex-1 flex items-center justify-center" style={{ paddingBottom: '20px' }}>
+          <JobStatusBarChart />
         </div>
-    )
+        
+        <div className="flex-1 flex items-center justify-center" style={{ paddingTop: '50px' }}>
+          <ApplicationsChart />
+        </div>
+      </div>
+    </div>
+  );
 }
